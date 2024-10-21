22fd20ae6878dcb4aec0e5929aed6c9061d58520
alexchino <129637403+alexchino@users.noreply.github.com>
2024-10-21 13:13:59 -0600

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['react.semantic-ui.com'],
+   
+  },
+};
+
+export default nextConfig;
---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ['react.semantic-ui.com'],
+   
+  },
+};
+
+export default nextConfig;
